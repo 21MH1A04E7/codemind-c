8f26cd058a40a3f46e260b0b2be0d5fd633bcaa2
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2022-01-23 01:57:06 +0530

    #include<stdio.h> int main(){     int num,i,j;     scanf("%d",&num);     for(i=1;i<=num;i++){         for(j=num;j>=1;j--){             printf("%d ",j);         }printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,11 +1,12 @@
+
 #include<stdio.h>
 int main(){
     int num,i,j;
     scanf("%d",&num);
-    for(i=num;i>=1;i--){
-        for(j=1;j<=i;j++){
-            printf("%d",j);
+    for(i=1;i<=num;i++){
+        for(j=num;j>=1;j--){
+            printf("%d ",j);
         }printf("
-");
+"); 
     }
 }
