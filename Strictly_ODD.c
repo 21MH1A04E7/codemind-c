f19109888784dbc22798ac4d89c1bf55a3ee5ee7
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2022-12-21 11:02:01 +0530

    #include<stdio.h> int main(){     int n,i,j,c=0;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++){         scanf("%d",&arr[i]);         if(arr[i]%2!=0){             c++;             arr[i]=0;         }     }     for(int i=1;i<n;i+=2){         for(int j=1;j<c;j++){             if(arr[i+1]==0){                 printf("False");                 return 0;             }         }     }     printf("True");     return 0; }

---
+++ Strictly_ODD.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,i,j,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        if(arr[i]%2!=0){
+            c++;
+            arr[i]=0;
+        }
+    }
+    for(int i=1;i<n;i+=2){
+        for(int j=1;j<c;j++){
+            if(arr[i+1]==0){
+                printf("False");
+                return 0;
+            }
+        }
+    }
+    printf("True");
+    return 0;
+}
