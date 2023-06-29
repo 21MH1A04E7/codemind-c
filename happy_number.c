e3fcd6eaf60293fb7f9a59a09f30be31bb7b6614
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-06-29 14:43:30 +0530

    #include<stdio.h> int digit(int n){     int sum=0;     while(n){         int r=n%10;         sum+=r*r;         n/=10;     }     return sum; } int check(int n){     int ans=digit(n);     if(ans==1){         return 1;     }     while(ans){         if(ans==1||ans==7){             return 1;         }         if(ans<10){             return 0;         }         ans=digit(ans);     }     return 1; } int main(){     int n;     scanf("%d",&n);     if(check(n)){         printf("True");     }else{         printf("False");     }
    }

--- happy_number.c
+++ happy_number.c
@@ -1,22 +1,36 @@
 #include<stdio.h>
-int main()
-{
-    int num;
-    scanf("%d",&num);
-    int sum=0,rem;
-    while(sum!=1&&sum!=4)
-    {
-        sum=0;
-        while(num!=0)
-        {
-            rem=num%10;
-            sum+=(rem*rem);
-            num/=10;
-        }
-        num=sum;
+int digit(int n){
+    int sum=0;
+    while(n){
+        int r=n%10;
+        sum+=r*r;
+        n/=10;
     }
-        if(sum==1)
+    return sum;
+}
+int check(int n){
+    int ans=digit(n);
+    if(ans==1){
+        return 1;
+    }
+    while(ans){
+        if(ans==1||ans==7){
+            return 1;
+        }
+        if(ans<10){
+            return 0;
+        }
+        ans=digit(ans);
+    }
+    return 1;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(check(n)){
         printf("True");
-        else
+    }else{
         printf("False");
+    }
+    
 }
