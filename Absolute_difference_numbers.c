638b1b492f507ab6133a322cbdb6a4c5659072b0
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-06-29 14:50:56 +0530

    #include<stdio.h> #include<stdlib.h> int rev(int n){     int r,sum=0;     while(n!=0){         r=n%10;         sum=sum*10+r;         n/=10;     }     return sum; } int last2(int n,int m){     int r,sum=0;     for(int i=1;i<=m;i++){         r=n%10;         sum=sum*10+r;         n/=10;     }     return sum; } int main(){     int n,m;     scanf("%d %d",&n,&m);     int a=last2(n,m);     int c=rev(n);     int b=last2(c,m);     int e=rev(a);     printf("%d",abs(e-b));     return 0; }

--- Absolute_difference_numbers.c
+++ Absolute_difference_numbers.c
@@ -1,21 +1,30 @@
 #include<stdio.h>
-#include <stdlib.h>
-#include<math.h>
-int main(){
-    int n,x;
-    scanf("%d %d",&n,&x);
-    int num=n;
-    int c=0;
-    int first=0,last=0;
-    int count=0;
-    while(num){
-        num/=10;
-        count++;
+#include<stdlib.h>
+int rev(int n){
+    int r,sum=0;
+    while(n!=0){
+        r=n%10;
+        sum=sum*10+r;
+        n/=10;
     }
-    int d=pow(10,x);
-    int num1=n%d;
-    int d2=pow(10,count-x);
-     n=n/d2;
-    //printf("%d",n);
-    printf("%d",abs(num1-n));
+    return sum;
+}
+int last2(int n,int m){
+    int r,sum=0;
+    for(int i=1;i<=m;i++){
+        r=n%10;
+        sum=sum*10+r;
+        n/=10;
+    }
+    return sum;
+}
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int a=last2(n,m);
+    int c=rev(n);
+    int b=last2(c,m);
+    int e=rev(a);
+    printf("%d",abs(e-b));
+    return 0;
 }
